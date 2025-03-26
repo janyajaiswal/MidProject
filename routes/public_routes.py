41ec39568fd190e22ebc0a39da8e9827f312ca09
janyajaiswal <jaiswaljanya@gmail.com>
2025-03-26 16:03:21 -0700

    fetching document details

--- routes/public_routes.py
+++ routes/public_routes.py
@@ -1,13 +1,21 @@
 from flask import Blueprint, jsonify, Response, current_app
 from bson import ObjectId
-from app import mongo
 
 public_bp = Blueprint("public", __name__)
 
 @public_bp.route("/public-files", methods=["GET"])
 def get_public_files():
-    files = mongo.db.files.find({}, {"_id": 0, "filename": 1, "type": 1, "role": 1})
-    return jsonify(list(files)), 200
+    cursor = current_app.db.files.find({})
+
+    files = []
+    for file in cursor:
+        file["_id"] = str(file["_id"])  # convert MongoDB _id
+        if "gridfs_id" in file:
+            file["gridfs_id"] = str(file["gridfs_id"])
+        files.append(file)
+
+    return jsonify(files), 200
+
 
 @public_bp.route("/download/<file_id>", methods=["GET"])
 def download_file(file_id):
